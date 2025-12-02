c6dc63cb0aa3791b00f442a8a45541988fbea29e
lijh8 <9011764+lijh8@users.noreply.github.com>
2025-12-03 04:56:13 +0800

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,14 +1,55 @@
 -- ~/.config/nvim/init.lua
 
--- https://neovim.io/doc/user/nvim.html#nvim-from-vim ,
-vim.opt.runtimepath:prepend("~/.vim")
-vim.opt.runtimepath:append("~/.vim/after")
-vim.opt.packpath = vim.opt.runtimepath:get()
-vim.cmd("source ~/.vimrc")
+-- multiple windows: C-W-S , C-W-V , :close , :term ,
+-- navigate: C-W-H , C-W-L , C-W-J , C-W-K ,
+-- term mode: C-\ C-n , or insert mode: i , a ,
 
--- terminal
--- split first, C-W-S , :term ,
--- enter and leave term mode: i , C-\ C-n ,
+-- :messages  -- check for messages with echom
+
+vim.cmd('filetype off')
+vim.cmd('syntax off')
+
+vim.opt.autoindent = true
+--vim.opt.colorcolumn = "80"
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.mouse = "a"  -- select to ctrl-c: win,linux:shift+mouse, mac:fn+mouse
+vim.opt.wrapscan = false
+vim.opt.number = true
+vim.opt.ruler = true  -- vim-tiny, show current line number at status line
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.updatetime = 200  -- CursorHold, tagbar
+
+-- autosave
+vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+  pattern = "*",
+  callback = function() vim.cmd("silent! update") end,
+})
+
+-- highlight current word
+vim.cmd([[
+  let old_isk = &isk | set isk=@,48-57,_,192-255 | autocmd CursorMoved,CursorMovedI * execute 'match Visual' (getline('.')[col('.')-1] =~# '\w' ? '/\<' . escape(expand('<cword>'), '/\') . '\>/' : '//') | let &isk = old_isk
+]])
+
+-- netrw
+vim.g.netrw_banner = 0
+vim.g.netrw_browse_split = 4
+vim.g.netrw_list_hide = [[.*\.swp$]]
+vim.g.netrw_liststyle = 3
+vim.g.netrw_winsize = 20
+vim.g.netrw_mousemaps = 0
+vim.keymap.set("n", "<F2>", "<cmd>Lexplore<CR>", { silent = true })
+
+-- Termdebug
+--vim.cmd("packadd termdebug")
+--vim.keymap.set("n", "<F5>", "<cmd>Termdebug<CR>", { silent = true })
+--vim.keymap.set("t", "<F10>", "<C-\\><C-N>:call TermDebugSendCommand('next')<CR>", { silent = true })
+--vim.keymap.set("t", "<F11>", "<C-\\><C-N>:call TermDebugSendCommand('step')<CR>", { silent = true })
+--vim.keymap.set("t", "<F12>", "<C-\\><C-N>:call TermDebugSendCommand('finish')<CR>", { silent = true })
+
 
 -- auto format on save by :w ,
 --vim.api.nvim_create_autocmd("BufWritePre", {
@@ -18,8 +59,10 @@ vim.cmd("source ~/.vimrc")
 --  end
 --})
 
+
 -- ~/.config/nvim/pack/nvim/start/nvim-lspconfig/lua/lspconfig/
 --require'lspconfig'.clangd.setup{}
+
 
 -- ~/.config/nvim/pack/nvim/start/aerial.nvim/lua/aerial/
 --require("aerial").setup({
@@ -31,7 +74,9 @@ vim.cmd("source ~/.vimrc")
 -- leader key is backslash \ so use \a in normal mode
 --vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle<CR>")
 
+
 -- ~/.config/nvim/pack/nvim/start/nvim-treesitter/lua/nvim-treesitter/
 --require'nvim-treesitter'.setup{}
+
 
 --
